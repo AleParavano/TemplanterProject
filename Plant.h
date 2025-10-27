dfc57dea7e4a7d2f70d04bfbcb5ebffd06af4771
Alessandro <u24713122@tuks.co.za>
2025-10-27 19:52:50 +0200

    Added different plants

--- Plant.h
+++ Plant.h
@@ -3,7 +3,8 @@
 #include "Subject.h"
 #include "GrowthCycle.h"
 
-class Plant: public Subject{
+class Plant: public Subject
+{
     public:
     Plant(std::string type,float growthRate);
     virtual ~Plant();
@@ -13,9 +14,60 @@ class Plant: public Subject{
     void attach(Observer* observer);
     void detach(Observer* observer);
     void setGrowthCycle(GrowthCycle* gc);
+
     protected:
     PlantState* state;
     GrowthCycle* growthCycle;
     std::string type;
     float growthRate;
+};
+
+class Carrot : public Plant {
+public:
+    Carrot() : Plant("Carrot", 1.1f) {}
+};
+
+class Tomato : public Plant {
+public:
+    Tomato() : Plant("Tomato", 1.4f) {}
+};
+
+class Sunflower : public Plant {
+public:
+    Sunflower() : Plant("Sunflower", 1.6f) {}
+};
+
+class Lettuce : public Plant {
+public:
+    Lettuce() : Plant("Lettuce", 1.0f) {}
+};
+
+class Potato : public Plant {
+public:
+    Potato() : Plant("Potato", 1.2f) {}
+};
+
+class Cucumber : public Plant {
+public:
+    Cucumber() : Plant("Cucumber", 1.3f) {}
+};
+
+class Pepper : public Plant {
+public:
+    Pepper() : Plant("Pepper", 1.5f) {}
+};
+
+class Strawberry : public Plant {
+public:
+    Strawberry() : Plant("Strawberry", 1.7f) {}
+};
+
+class Corn : public Plant {
+public:
+    Corn() : Plant("Corn", 1.8f) {}
+};
+
+class Pumpkin : public Plant {
+public:
+    Pumpkin() : Plant("Pumpkin", 2.0f) {}
 };
