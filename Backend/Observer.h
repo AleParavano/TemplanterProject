d1c9b2568ae2db52e5ca0978b9ee506aeb71c0c7
u24754120 <u24754120@tuks.co.za>
2025-10-28 15:22:36 +0200

    changed observer

--- Backend/Observer.h
+++ Backend/Observer.h
@@ -4,12 +4,8 @@
 class Observer{
     public:
     virtual void update(Plant* Plant)=0;
-    Observer(){
-        id=ObserverCount++;
-    }
-    bool operator==(Observer* observer){
-        return this->id==observer->id;
-    }
+    Observer();
+    bool operator==(Observer* observer);
     virtual ~Observer(){}
     protected:
     static int ObserverCount;
